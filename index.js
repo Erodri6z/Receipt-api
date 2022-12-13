281c16ffa2e34615188d3039ff7109fbf876f762
Erodri6z <erikrodriguez2417@gmail.com>
2022-12-13 15:05:43 -0600

    add a route to create an id for reciept

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const app = require('express')()
-const PORT = 8080
+const express = require('express');
+const app = express();
+const PORT = 8080;
 
-app.listen(
-  PORT,
-  () => console.log(`Its live on ${PORT}`)
-)
+
+app.use( express.json() );
+
+app.listen(PORT,
+  () => console.log('its running')
+); 
+
+app.get('/reciepts', (req, res) => {
+  res.status(200).send({
+    "retailer": "Walgreens",
+    "purchaseDate": "2022-01-02",
+    "purchaseTime": "08:13",
+    "total": "2.65",
+    "items": [
+        {"shortDescription": "Pepsi - 12-oz", "price": "1.25"}
+    ]
+  })
+});
+
+app.post('/reciepts/process', (req, res) => {
+  const  id  = parseInt(Math.floor(Math.random() * Date.now()))
+  const { retailer } = req.body
+  const { purchaseDate } = req.body
+  const { purchaseTime } = req.body
+  const { total } = req.body
+  const  items  = req.body
+  console.log( req.body )
+  res.send({
+    id: `${id}`,
+    reciept : req.body
+  })
+});
