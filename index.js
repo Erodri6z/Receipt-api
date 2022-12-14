daa8c09ba0b2c744cd9728c153f1aa9455252dbc
Erodri6z <erikrodriguez2417@gmail.com>
2022-12-14 04:17:59 -0600

    add a function to search and list all reciepts

--- index.js
+++ index.js
@@ -9,28 +9,58 @@ app.listen(PORT,
   () => console.log('its running')
 ); 
 
-app.get('/reciepts', (req, res) => {
-  res.status(200).send({
-    "retailer": "Walgreens",
-    "purchaseDate": "2022-01-02",
-    "purchaseTime": "08:13",
-    "total": "2.65",
-    "items": [
-        {"shortDescription": "Pepsi - 12-oz", "price": "1.25"}
+const reciepts = [
+  { 
+    id : 398916030109,
+    retailer: "Walgreens",
+    purchaseDate: "2022-01-02",
+    purchaseTime: "08:13",
+    total: "2.65",
+    items: [
+      {
+        shortDescription: "Pepsi - 12-oz",
+        price: "1.25"
+      }
     ]
-  })
+  },
+  {
+    id : 263620709779,
+    retailer: "Target",
+    purchaseDate: "2022-01-02",
+    purchaseTime: "13:13",
+    total: "1.25",
+    items: [
+      {
+        shortDescription: "Pepsi - 12-oz",
+        price: "1.25"
+      }
+    ]
+  }
+]
+
+app.get('/reciepts', (req, res) => {
+  res.status(200).send(reciepts)
 });
 
 app.post('/reciepts/process', (req, res) => {
-  const  id  = parseInt(Math.floor(Math.random() * Date.now()))
-  const { retailer } = req.body
-  const { purchaseDate } = req.body
-  const { purchaseTime } = req.body
-  const { total } = req.body
-  const  items  = req.body
-  console.log( req.body )
-  res.send({
-    id: `${id}`,
-    reciept : req.body
-  })
+  const reciept = {
+    id: parseInt(Math.floor(Math.random() * Date.now())),
+    retailer : req.body.retailer,
+    purchaseDate: req.body.purchaseDate,
+    purchaseTime: req.body.purchaseDate,
+    total: req.body.total,
+    items: req.body.items
+    }
+  // const { retailer } = req.body.retailer
+  // const { purchaseDate } = req.body.purchaseDate
+  // const { purchaseTime } = req.body.purchaseTime
+  // const { total } = req.body.total
+  // const  items  = req.body.items
+
+  res.send(reciept)
 });
+
+app.get(`/receipts/:id`, (req, res) => {
+  const reciept = reciept.find(r => r.id === parseInt(req.params.id))
+  res.status(200).send(reciept)
+})
